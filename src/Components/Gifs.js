62df1ff6e26f32aa0d7c68ebca18bbbf45314f4f
DesmonFur <desfurtick@gmail.com>
2020-01-20 23:01:18 -0700

    removed some styling

--- src/Components/Gifs.js
+++ src/Components/Gifs.js
@@ -4,36 +4,38 @@ import axios from "axios";
 export class Gifs extends Component {
   state = {
     gifs: [],
-    gif_url:''
+    gif_url: ""
   };
 
   componentDidMount() {
-    axios
-      .get(
-'/api/getGifs'      )
-      .then(res => {
-        console.log(res);
-        this.setState({
-          gifs: res.data
-        });
+    axios.get("/api/getGifs").then(res => {
+      console.log(res);
+      this.setState({
+        gifs: res.data
       });
+    });
   }
 
   render() {
     console.log(this.state.gifs);
-    console.log(this.state.gif_url)
+    console.log(this.state.gif_url);
     const { gifs } = this.state;
 
     const giphys = gifs.map(gif => {
       return (
-        <div key={gif.gif_id}>
-          <h1>{gif.gif_title}</h1>
-          <img src={gif.gif_url} alt="gifs" />
+        <div className="gif-container" key={gif.gif_id}>
+          <h1 className="gif-title">{gif.gif_title}</h1>
+          <img src={gif.gif_url} className="gifs" alt="gifs" />
         </div>
       );
     });
 
-    return <div>{giphys}</div>;
+    return (
+      <div className="space">
+        <h1 className="Title"> Giphy Like</h1>
+        {giphys}
+      </div>
+    );
   }
 }
 
